6c2085753d4bc5697e6b0c227e719e92ea607892
Kumar, Sachin <Sachin.Kumar@vector.com>
2026-02-21 04:04:10 +0530

    Update Udmy_OOPs.py

--- Udmy_OOPs.py
+++ Udmy_OOPs.py
@@ -1,21 +1,3 @@
-picture = [
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0]
-]
-
-# print(len(picture[1]))
-
-
-# for row in range(len(picture)):
-#     for column in range(len(picture[row])):
-#         print(picture[row][column],end=" ")
-#     print("\n")
-
-
 for row in range(len(picture)):
     for column in range(len(picture[row])):
         if (picture[row][column] == 1):
